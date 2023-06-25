d59a2b945def4dd3058bed22d930a6bbbcd365d0
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-06-25 16:15:31 +0300

    yeni renk eklendi

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
-import "../style/login.css"
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { addUser } from '../redux/slice'
-
+import React, { useEffect, useState } from "react";
+import "../style/login.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../redux/slice";
 
 const Login = () => {
-  const [inputName, setInputName] = useState()
-  const [inputPassword, setInputPassword] = useState()
+  const [inputName, setInputName] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const { data } = useSelector((state) => state.users);
 
-  const navigate = useNavigate()
-  const dispacth = useDispatch()
- 
-const {data} = useSelector((state)=>state.users)
-const enter = () =>{
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(data));
+  }, [data]);
+
+  const navigate = useNavigate();
+  const dispacth = useDispatch();
+
+  const enter = () => {
+    console.log(data);
+    navigate("/home");
+  };
+  const register = () => {
+    if (inputName !== "" && inputPassword !== "") {
+      dispacth(addUser({ name: inputName, password: inputPassword }));
+    }
+  };
   console.log(data);
-  navigate("/home")
-}
-const register = () =>{
-  if(inputName !== "" && inputPassword !== ""){
-    dispacth(addUser({name:inputName,password:inputPassword}))
-  }
-}
 
   return (
-    <div className='login'>
-        <div className='loginScreen'>
-         
-             <div className='loginImage'>
-             </div>
-           
-            <div className='loginInput'>
-            <input type='text' placeholder='Kullanıcı adı girin:' onChange={(e)=>setInputName(e.target.value)}/>
-            <input type='password' placeholder='Şifre girin:' onChange={(e)=>setInputPassword(e.target.value)}/>
-            <button onClick={enter}>Giriş</button>
+    <div className="login">
+      <div className="loginScreen">
+        <div className="loginImage"></div>
 
-            
-            <button onClick={register}>Kayıt</button>
+        <div className="loginInput">
+          <input
+            type="text"
+            placeholder="Kullanıcı adı girin:"
+            onChange={(e) => setInputName(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Şifre girin:"
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
+          <button onClick={enter}>Giriş</button>
 
-            </div>
-            
+          <button onClick={register}>Kayıt</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
