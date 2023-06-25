12cdf4c3748b8158cbb71c7d18566661fe1fdc79
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-06-25 12:29:11 +0300

    login sayfası style işlemleri

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,20 @@ import "../style/login.css"
 
 const Login = () => {
   return (
-    <div className='Login'></div>
+    <div className='login'>
+        <div className='loginScreen'>
+         
+             <div className='loginImage'>
+             </div>
+           
+            <div className='loginInput'>
+            <input type='text' placeholder='Kullanıcı adı girin:'/>
+            <input type='password' placeholder='Şifre girin:'/>
+            <button>Giriş</button>
+            </div>
+            
+        </div>
+    </div>
   )
 }
 
