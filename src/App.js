1d1851124ac62bf4125bf260060ed483afe35590
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-06-25 11:55:42 +0300

    css dosyaları

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import NewComment from './pages/NewComment';
 
 function App() {
   return (
@@ -8,7 +12,7 @@ function App() {
      <BrowserRouter>
      <Routes>
       <Route path='/' element={<Login/>} />
-      <Route path='/home' element={<Login/>} />
+      <Route path='/home' element={<Home/>} />
       <Route path='/product' element={<Product/>} />
       <Route path='newcomment' element={<NewComment/>} />
      </Routes>
