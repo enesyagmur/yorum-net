87a35dd222e58733048d7b055ed3974aa775551f
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-06-25 11:42:20 +0300

    renk tanımlama ve router yapısı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     Hello world
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Login/>} />
+      <Route path='/home' element={<Login/>} />
+      <Route path='/product' element={<Product/>} />
+      <Route path='newcomment' element={<NewComment/>} />
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
